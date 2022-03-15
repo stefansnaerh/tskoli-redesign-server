c4ae267c2f75484a62835eed3734b3834e3d48c2
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2022-03-15 16:22:58 +0000

    fixed the timout stuff

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,4 @@
-
+let timeout = 0;
 const isAdmin = (req, res, next) => {
   // Check if user is admin or is using "su",
   // i.e. acting as another user
@@ -25,18 +25,20 @@ const isNotAuthenticated = (req, res, next) => {
   next();
 };
 const rateLimit = (req, res, next) => {
- /* if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     return next();
   }
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const key = `${ip}-${req.originalUrl}`;
   const limit = 40;
   const expireTime = 1000 * 60 * 60; // 1 hour
-  clearTimeout(req.session.rateLimitTimeout);
-  req.session.rateLimitTimeout = setTimeout(() => {
+  clearTimeout(timeout);
+  
+  timeout = setTimeout(() => {
     delete req.session[key];
   }, expireTime);
   // Check if user has exceeded their rate limit
+  
   if (req.session[key]) {
     if (req.session[key] >= limit) {
       
@@ -48,7 +50,7 @@ const rateLimit = (req, res, next) => {
     req.session[key] = 1;
   }
 
-*/
+
   return next();
 }
 
