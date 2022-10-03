04cff2a4b9c42ecc410169da5943487e39026bbf
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2022-10-03 14:04:02 +0000

    changed the type of date inputs to Date and added the updatedAt property

--- model/Calendar.js
+++ model/Calendar.js
@@ -1,40 +1,43 @@
 const mongoose = require("mongoose");
 
 const calendarSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-},
+    _id: {
+        type: String,
+    },
 
-Category: {
-    type: String, //what kind of event is this (is it a guide, trip, meeting etc...?)
-},
+    Category: {
+        type: String, //what kind of event is this (is it a guide, trip, meeting etc...?)
+    },
 
-Attending: {
-    type: Array, //array of objectIds - only registered users can attend the events
-},
+    Attending: {
+        type: Array, //array of objectIds - only registered users can attend the events
+    },
 
-Title: 
-{
-    type: String, //name of the event
-},
+    Title: 
+    {
+        type: String, //name of the event
+    },
 
-Owner: {
-    type: String, //who created this event
-},
+    Owner: {
+        type: String, //who created this event
+    },
 
-Description: {
-    type: String, //description of the event
-},
+    Description: {
+        type: String, //description of the event
+    },
 
 
-Starting: {
-    type: Array, //when does the event start
-},
+    Starting: {
+        type: Date, //when does the event start
+    },
 
-Ending: {
-    type: Array, //when does the event end
-}
+    Ending: {
+        type: Date, //when does the event end
+    },
 
+    updatedAt:{
+        type: Date,
+    },
 }); 
 
 module.exports = mongoose.model("Calendar", calendarSchema);
