6ad3a0dcf03283b13961dfd0a46005c9df425cc7
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2022-10-27 13:23:56 +0000

    took out the comments

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -6,47 +6,18 @@ const Review = require("../model/Review");
 const controller = {};
 
 controller.getAssignments = async (req, res) => {
-    // const guides = (await axios.get(`${process.env.CMS_URL}/guides/short`)).data;
-    // const guides = await Guides.find({});
+
     const assignmentReturns = await AssignmentReturn.find().populate("sender", "name");
     const reviews = await Review.find();
 
-    //adding assisignments to the assignmentReturns:
-    /*const returnsWithGuides = assignmentReturns.map((assignmentReturn) => {
-        const simplifiedReturn = assignmentReturn.toObject();
-
-        const assignment = guides.find(
-            (guide) => guide["_id"] === simplifiedReturn.assignment.toString()
-        ) || {};
-        const project = { project: assignment.project, title: assignment.Title };
-        simplifiedReturn.assignment = project;
-        return simplifiedReturn;
-    });*/
-    //only get recommended reviews to publish
     const recommendedReviews = reviews.filter(review => review.vote === "recommend")
-    //adding recommended rewiews to the returns :
+    //filtering only returns that have been recommended:
     const recommendedReturns = assignmentReturns.filter( ret => {
         const reviewsFound = recommendedReviews.findIndex( review => ret["_id"].toString() === review.assignmentReturn.toString()) 
         console.log(reviewsFound);
         return reviewsFound != -1;
      })
-    /*recommendedReviews.forEach((rawReview) => { // go through all the recommended reviews
-        const review = rawReview.toObject(); // turn each review into object
-        const index = returnsWithGuides.findIndex( // index is the particular review, inside assignmentReturns array
-            (r) => r["_id"].toString() === review.assignmentReturn.toString()
-        );
-
-        try {
-            if (!returnsWithGuides[index].reviews) {
-                returnsWithGuides[index].reviews = [];
-            }
-            const vote = { vote: review.vote };
-            returnsWithGuides[index].reviews.push(vote); // only keep vote
-        } catch (err) {
-            // In case an assignment return was
-            // deleted, it won't be found
-        }
-    });*/
+  
 
     return res.send(recommendedReturns);
 };
